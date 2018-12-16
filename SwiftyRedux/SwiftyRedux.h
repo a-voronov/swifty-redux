8e0ae51069e1f3f8df39082f44ab2e37771b09af
Alexander Voronov <voronovaleksandr91@gmail.com>
2018-12-16 02:49:52 +0200

    Initial Commit

---
+++ SwiftyRedux/SwiftyRedux.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyRedux.h
+//  SwiftyRedux
+//
+//  Created by Alexander Voronov on 12/16/18.
+//  Copyright © 2018 Alex Voronov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyRedux.
+FOUNDATION_EXPORT double SwiftyReduxVersionNumber;
+
+//! Project version string for SwiftyRedux.
+FOUNDATION_EXPORT const unsigned char SwiftyReduxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyRedux/PublicHeader.h>
+
+
