eb6031b8fefab6400108ecbf887a2d5017a96da2
Alexander Voronov <voronovaleksandr91@gmail.com>
2019-01-07 20:23:14 +0200

    created pod for framework

---
+++ Example/Pods/Target Support Files/SwiftyRedux/SwiftyRedux-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftyRedux : NSObject
+@end
+@implementation PodsDummy_SwiftyRedux
+@end
---
+++ Example/Tests/Tests.swift
@@ -0,0 +1,12 @@
+//
+//  Tests.swift
+//  SwiftyRedux_Tests
+//
+//  Created by Alexander Voronov on 1/7/19.
+//  Copyright © 2019 SwiftyRedux. All rights reserved.
+//
+
+import XCTest
+
+class Tests: XCTestCase {
+}
