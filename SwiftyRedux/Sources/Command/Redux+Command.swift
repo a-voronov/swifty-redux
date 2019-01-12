13546f30b897d75c9d994222182db6eae5101651
Alexander Voronov <voronovaleksandr91@gmail.com>
2019-01-12 04:08:28 +0200

    added command integration tests

--- SwiftyRedux/Sources/Command/Redux+Command.swift
+++ SwiftyRedux/Sources/Command/Redux+Command.swift
@@ -4,15 +4,15 @@
 
 extension Store {
     @discardableResult
-    public func subscribe(on queue: DispatchQueue? = nil, observer: Command<State>) -> Disposable {
-        return subscribe(on: queue, observer: observer.execute)
+    public func subscribe(on queue: DispatchQueue? = nil, _ command: Command<State>) -> Disposable {
+        return subscribe(on: queue, observer: command.execute)
     }
 }
 
 extension Store where State: Equatable {
     @discardableResult
-    public func subscribe(on queue: DispatchQueue? = nil, skipRepeats: Bool = false, observer: Command<State>) -> Disposable {
-        return subscribe(on: queue, skipRepeats: skipRepeats, observer: observer.execute)
+    public func subscribe(on queue: DispatchQueue? = nil, skipRepeats: Bool = false, _ command: Command<State>) -> Disposable {
+        return subscribe(on: queue, skipRepeats: skipRepeats, observer: command.execute)
     }
 }
 
@@ -20,27 +20,27 @@ extension Store where State: Equatable {
 
 extension Observable {
     @discardableResult
-    public func subscribe(on observingQueue: DispatchQueue? = nil, observer: Command<Value>) -> Disposable {
-        return subscribe(on: observingQueue, observer: observer.execute)
+    public func subscribe(on observingQueue: DispatchQueue? = nil, _ command: Command<Value>) -> Disposable {
+        return subscribe(on: observingQueue, observer: command.execute)
     }
 }
 
 // MARK: - Observer
 
 extension Observer {
-    public convenience init(queue: DispatchQueue? = nil, update: Command<Value>) {
-        self.init(queue: queue, update: update.execute)
+    public convenience init(queue: DispatchQueue? = nil, _ command: Command<Value>) {
+        self.init(queue: queue, update: command.execute)
     }
 }
 
 // MARK: - Disposable
 
 extension Disposable {
-    public convenience init(id: String? = nil, action: Command<Void>) {
-        self.init(id: id, action: { action.execute() })
+    public convenience init(id: String? = nil, _ command: Command<Void>) {
+        self.init(id: id, action: { command.execute() })
     }
 
-    public convenience init(id: String? = nil, action: Command<Disposable?>) {
-        self.init(id: id, action: action.execute)
+    public convenience init(id: String? = nil, _ command: Command<Disposable?>) {
+        self.init(id: id, action: command.execute)
     }
 }
